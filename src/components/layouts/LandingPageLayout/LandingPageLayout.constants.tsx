fc19f92d15daed8c44c3044b733ccb28b39165a5
Alize21 <pusikentut@gmail.com>
2026-01-28 20:33:15 +0700

    feat: add footer

--- src/components/layouts/LandingPageLayout/LandingPageLayout.constants.tsx
+++ src/components/layouts/LandingPageLayout/LandingPageLayout.constants.tsx
@@ -1,3 +1,5 @@
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+
 const NAV_ITEMS = [
   { label: "Home", href: "/" },
   { label: "Explore", href: "/event" },
@@ -8,4 +10,27 @@ const BUTTON_ITEMS = [
   { label: "Login", href: "/auth/login", variant: "solid" },
 ];
 
-export { NAV_ITEMS, BUTTON_ITEMS };
+const SOCIAL_ITEMS = [
+  {
+    label: "Facebook",
+    href: "https://www.facebook.com/nightly.callia/",
+    icon: <FaFacebook />,
+  },
+  {
+    label: "Instagram",
+    href: "https://www.instagram.com/nightly_callia21/",
+    icon: <FaInstagram />,
+  },
+  {
+    label: "Twitter",
+    href: "https://x.com/_Alize21",
+    icon: <FaTwitter />,
+  },
+  {
+    label: "YouTube",
+    href: "https://www.youtube.com/@igalizexmo_1220",
+    icon: <FaYoutube />,
+  },
+];
+
+export { NAV_ITEMS, BUTTON_ITEMS, SOCIAL_ITEMS };
